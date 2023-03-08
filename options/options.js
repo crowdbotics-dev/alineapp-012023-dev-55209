f5d07d20fe0f6ae82948709341ea149ad615bd5d
Aline Tavares <alaisgomes@gmail.com>
2023-03-08 16:14:32 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "alineapp-012023-dev-55209",
-  url: "https://alineapp-012023-dev-55209.botics.co",
-  api: "https://alineapp-012023-dev-55209.botics.co/api/v1"
+  "name": "alineapp-012023-dev-55209",
+  "url": "https://alineapp-012023-dev-55209.botics.co",
+  "api": "https://alineapp-012023-dev-55209.botics.co/api/v1",
+  "initialRoute": "BlankScreen3325"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "copy": "Routes available!"
   }
 };
--- screens/BlankScreen3325/index.js
+++ screens/BlankScreen3325/index.js
@@ -1,10 +1,9 @@
-
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const BlankScreen3325 = () => {
   return <View style={_styles.BlankScreen3325}>
-      </View>;
+      <View style={_styles.RgYZZqia}></View></View>;
 };
 
 export default BlankScreen3325;
@@ -15,5 +14,12 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  RgYZZqia: {
+    height: 248,
+    width: 308,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
